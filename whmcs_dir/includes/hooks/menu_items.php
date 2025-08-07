e9ba51eb33d4e446d3cc1e0fdf5d07d567dba106
Aman Kumar <aman.kumar01@shinedezign.com>
2025-08-07 14:11:16 +0530

    "update hook"

--- whmcs_dir/includes/hooks/menu_items.php
+++ whmcs_dir/includes/hooks/menu_items.php
@@ -9,24 +9,17 @@ if (!defined("WHMCS")) {
 add_hook('ClientAreaPrimaryNavbar', 1, function (MenuItem $primaryNavbar) {
 
     try {
-        /* Header Items after login */
         if (isset($_SESSION['uid']) && !empty($_SESSION['uid'])) {
     
-            // Remove "Open Ticket" menu item
             if (!is_null($primaryNavbar->getChild('Open Ticket'))) {
                 $primaryNavbar->removeChild('Open Ticket');
             }
     
-            // Add Open Ticket in Support menu item
             if (!is_null($primaryNavbar->getChild('Support'))) {
                 $primaryNavbar->getChild('Support')->addChild('Open Ticket')->setUri('/submitticket.php');
-            }
+            }            
         }
     
-
-        /* Header Items before login */
-
-        // Add new menu item "Support"
         if (is_null($primaryNavbar->getChild('Support'))) {
             $primaryNavbar->addChild('Support', [
                 'label' => 'Support',
@@ -35,13 +28,10 @@ add_hook('ClientAreaPrimaryNavbar', 1, function (MenuItem $primaryNavbar) {
             ]);
         }
     
-
-        // Replace the main menu items as sub menues in Support menu
         $supportMenu = $primaryNavbar->getChild('Support');
 
         if (!is_null($supportMenu)) {
     
-            // Remove items
             if (!is_null($supportMenu)) {
                 $primaryNavbar->removeChild('Announcements');
             }
@@ -52,10 +42,30 @@ add_hook('ClientAreaPrimaryNavbar', 1, function (MenuItem $primaryNavbar) {
                 $primaryNavbar->removeChild('Contact Us');
             }
     
-            // Add items
             $supportMenu->addChild('Announcements')->setUri('/index.php?rp=/announcements');
             $supportMenu->addChild('Knowledgebase')->setUri('/index.php?rp=/knowledgebase');
             $supportMenu->addChild('Contact Us')->setUri('/contact.php');
+        }
+
+        // Affiliate menu
+        if(!is_null($primaryNavbar->getChild("Affiliates"))) {
+            $primaryNavbar->removeChild("Affiliates");
+
+            $primaryNavbar->addChild('Affiliates', [
+                'label' => 'Affiliates',
+                'uri' => '#',
+                'order' => 99,
+            ]);
+
+            $affiliateMenu = $primaryNavbar->getChild("Affiliates");
+
+            $affiliateMenu->addChild("Affiliates")->setUri("/affiliates.php");
+            $affiliateMenu->addChild("Affiliates Calculator")->setUri("/affiliate-calculator.php");
+        }
+
+        // Home page to WP 
+        if (empty($_SESSION['uid'])) {
+            $primaryNavbar->getChild("Home")->setUri("https://affiliatehosting.ai/");
         }
         
     } catch (Exception $e) {
@@ -65,44 +75,49 @@ add_hook('ClientAreaPrimaryNavbar', 1, function (MenuItem $primaryNavbar) {
 });
 
 
-add_hook('ClientAreaHeaderOutput', 1, function($vars) {
+add_hook('ClientAreaHeaderOutput', 1, function ($vars) {
 
     try {
 
         return <<<HTML
-            <script>
-                $(document).ready(function () {
-                    $(".div-service-item").each(function () {
-                        var dataHref = $(this).attr("data-href");
-                        var match = dataHref.match(/id=(\\d+)/);
-    
-                        if (match && match[1]) {
-                            var serviceId = match[1];
-                            var loginUrl = "?action=productdetails&id=" + serviceId + "&dosinglesignon=1";
-    
-                            var loginLink = $("<a>", {
-                                class: "btn btn-default btn-sm mr-2",
-                                href: loginUrl,
-                                target: "_blank",
-                                text: "Log in to Control Panel"
+                    <script>
+                        $(document).ready(function () {
+
+                            $(".div-service-item").each(function () {
+                                var dataHref = $(this).attr("data-href");
+                                var match = dataHref && dataHref.match(/id=(\\d+)/);
+
+                                if (match && match[1]) {
+                                    var serviceId = match[1];
+                                    var loginUrl = "?action=productdetails&id=" + serviceId + "&dosinglesignon=1";
+
+                                    var loginLink = $("<a>", {
+                                        class: "btn btn-default btn-sm mr-2",
+                                        href: loginUrl,
+                                        target: "_blank",
+                                        text: "Log in to Control Panel"
+                                    });
+
+                                    $(this).find(".div-service-buttons .btn-view-details").before(loginLink);
+                                }
                             });
-    
-                            $(this).find(".div-service-buttons .btn-view-details").before(loginLink);
-                        }
-                        
-                        $(".div-service-buttons a.btn").on("click", function(event) {
-                            event.stopPropagation();
+
+
+                            $(".div-service-buttons a.btn").on("click", function(event) {
+                                event.stopPropagation();
+                            });
                         });
-                    });
-                });
-            </script>
-        HTML;
+                        
+                    </script>
+                    HTML;
 
     } catch (Exception $e) {
-        logActivity("Error in ClientAreaHeaderOutput. Error: ". $e->getMessage());
+        logActivity("Error in ClientAreaHeaderOutput. Error: " . $e->getMessage());
     }
 
 });
+
+
 
 
 
